82c6b75593ce381343c47d8d3f060c4d1b31a07b
Kevin Compton <klcompt@gmail.com>
2013-10-14 17:47:03 -0500

    Initial commit after creating app and adding BDD TDD support.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Messaging::Application.config.secret_key_base = 'd09fa10bbae7724a6a59c9d84572c966f122a1c11f08a829c6a6541eefa315ae1c1a4d4a8c2a0687998585f3b4ceec091f9e1f438862aa294e9cd4d0663296d1'
