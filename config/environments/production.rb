10baca2465b5a9f6db9aa132521959caf7626237
Kevin Compton <klcompt@gmail.com>
2013-10-20 15:02:56 -0500

    Attempt 2 at stopping compression of javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ Messaging::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = false
   #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
