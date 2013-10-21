e8211514431aa74f5f48c1adb32a3f7d76786c6a
Kevin Compton <klcompt@gmail.com>
2013-10-21 18:43:08 -0500

    Skipping verify auth token for now.

--- app/controllers/twilio/messages_controller.rb
+++ app/controllers/twilio/messages_controller.rb
@@ -1,4 +1,6 @@
 class Twilio::MessagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def initiate
     logger.error "initiate called with#{ params.inspect}"
   end
