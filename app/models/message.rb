52745ac3c470066ffca909703c490d0eb33ec340
Kevin Compton <klcompt@gmail.com>
2013-10-21 19:43:47 -0500

    Adding an active scope to use when searching for message to play

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
+
+
+  scope :active, where(rescended: false)
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,4 +3,23 @@ require 'spec_helper'
 describe Message do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
+
+  describe '#active scope' do
+    let!(:active_message1) { FactoryGirl.create :message }
+    let!(:rescended_message1) { FactoryGirl.create :message, rescended: true }
+    let!(:active_message2) { FactoryGirl.create :message }
+    let!(:rescended_message2) { FactoryGirl.create :message, rescended: true }
+    let!(:active_message3) { FactoryGirl.create :message }
+
+    it 'has 5 total messages' do
+      Message.unscoped.count.should == 5
+    end
+
+    it 'returns only the three active messages' do
+      messages = Message.active.all
+      messages[0].should == active_message1
+      messages[1].should == active_message2
+      messages[2].should == active_message3
+    end
+  end
 end
